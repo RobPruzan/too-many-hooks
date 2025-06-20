51700a72392f82e6e023f679938106a37dd56d15
RobPruzan <rapruzan@buffalo.edu>
2025-06-19 18:01:53 -0700

    repro

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,106 @@
-import Image from "next/image";
+"use client";
+import { Fragment, startTransition, use, useMemo } from "react";
+import { useEffect, useState } from "react";
+import { Suspense, Component } from "react";
 
-export default function Home() {
+export const ClientSuspense = ({ children }: { children: React.ReactNode }) => {
+  return <Suspense fallback={<div>loading</div>}>{children}</Suspense>;
+};
+
+interface ErrorBoundaryProps {
+  children: React.ReactNode;
+  fallback?: React.ReactNode;
+}
+
+interface ErrorBoundaryState {
+  hasError: boolean;
+  error: Error | null;
+}
+
+export class ErrorBoundary extends Component<
+  ErrorBoundaryProps,
+  ErrorBoundaryState
+> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = { hasError: false, error: null };
+  }
+
+  static getDerivedStateFromError(error: Error): ErrorBoundaryState {
+    return { hasError: true, error: error };
+  }
+
+  componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {}
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        this.props.fallback || (
+          <div>
+            Error: {this.state.error?.message || "Something went wrong."}
+          </div>
+        )
+      );
+    }
+
+    return this.props.children;
+  }
+}
+
+export default function Page() {
+  const [promise, setTrigger] = useState<Promise<any> | null>(null);
+  const _ = promise ? use(promise) : promise;
+  useMemo(() => {}, []);
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    <Fragment>
+      <Redirect setTrigger={setTrigger} />
+      <ClientSuspense>
+        <ErrorBoundary>
+          <Bomb />
+        </ErrorBoundary>
+      </ClientSuspense>
+    </Fragment>
   );
 }
+
+const Bomb = () => {
+  throw new Error("boom");
+  return <div>hi</div>;
+};
+
+const Redirect = ({
+  setTrigger,
+}: {
+  setTrigger: (promise: Promise<any>) => void;
+}) => {
+  const [state, setState] = useState<string>("v1");
+
+  const dispatchPromise = () => {
+    console.log("dispatch promise");
+
+    startTransition(() => {
+      let resolver: (value: any) => void;
+      let promise = new Promise((res) => {
+        resolver = res;
+      });
+      setTrigger(promise);
+      setTimeout(() => {
+        resolver("hi");
+      }, 100);
+    });
+  };
+
+  useEffect(() => {
+    setState("/versions/v2");
+  }, []);
+
+  useEffect(() => {
+    // this works
+    // setTimeout(() => {
+    //   dispatchPromise();
+    // });
+    dispatchPromise();
+  }, [state]);
+
+  return null;
+};
